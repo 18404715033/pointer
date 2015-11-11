5416e86703b03f909fc89bf3f144236c7df6a882
18404715033 <965630447@qq.com>
2015-11-11 21:29:39 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by 李伯阳 on 15/11/11.
+//  Copyright © 2015年 李伯阳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
